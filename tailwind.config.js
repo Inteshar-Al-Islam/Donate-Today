2b0dd58b7127aa3154c0155529761c27ac21da94
Inteshar-Al-Islam <deadperson2047@gmail.com>
2024-09-10 17:20:25 +0600

    updated the header part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ['./index.html'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html'],
+  content: ['./index.html'], // Ensure this matches your HTML file location
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
